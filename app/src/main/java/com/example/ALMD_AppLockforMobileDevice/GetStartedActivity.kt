b35b46b56ad1d2106b9d97afad931c9c5bdfcb8a
RainC07 <rainchan07@gmail.com>
2022-12-27 13:52:55 +0800

    v6.0 ✓ Get Started ✓ Developer Notes ✓ Master PIN Setup ✓ Mobile Applications Selection ✓ Main Menu ✓ Lock Screen UI ✗ Customization (Optional)
    Completed
    ✓ Switch for enabling and preventing the use of biometrics
    
    In progress
    - Lock apps listed in Shared Preferences file

--- app/src/main/java/com/example/ALMD_AppLockforMobileDevice/GetStartedActivity.kt
+++ app/src/main/java/com/example/ALMD_AppLockforMobileDevice/GetStartedActivity.kt
@@ -14,9 +14,15 @@ class GetStartedActivity : AppCompatActivity() {
         val sharedPref_masterPin = getSharedPreferences("masterPin", MODE_PRIVATE)
         val master_Pin: String? = sharedPref_masterPin.getString("masterPIN", null)
 
-        if (master_Pin != null) {
+        val sharedPref_toggleBiometrics = getSharedPreferences("allowBiometrics", MODE_PRIVATE)
+        val onOrOff: Boolean = sharedPref_toggleBiometrics.getBoolean("onOrOff", true)
+
+        if (master_Pin != null && onOrOff) {
             val toUserVerification = Intent(this,UserVerificationActivity::class.java)
             startActivity(toUserVerification)
+        } else if (master_Pin != null) {
+            val toEnterPin = Intent(this,EnterPINActivity::class.java)
+            startActivity(toEnterPin)
         }
 
         val getStartedNextBtn = findViewById<Button>(R.id.getStarted_nextBtn)
--- app/src/main/java/com/example/ALMD_AppLockforMobileDevice/MainMenuActivity.kt
+++ app/src/main/java/com/example/ALMD_AppLockforMobileDevice/MainMenuActivity.kt
@@ -2,25 +2,39 @@ package com.example.ALMD_AppLockforMobileDevice
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import android.graphics.drawable.Drawable
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
 
 class MainMenuActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
 
+        val toggleBiometrics: Switch = findViewById(R.id.toggleBiometrics)
+
         val sharedPref_lockedAppsList = getSharedPreferences("lockedAppsList", MODE_PRIVATE)
         val editor_lockedAppsList = sharedPref_lockedAppsList.edit()
+
+        val sharedPref_toggleBiometrics = getSharedPreferences("allowBiometrics", MODE_PRIVATE)
+        val editor_toggleBiometrics = sharedPref_toggleBiometrics.edit()
+        val onOrOff: Boolean = sharedPref_toggleBiometrics.getBoolean("onOrOff", false)
+
+        toggleBiometrics.isChecked = onOrOff
+
+        toggleBiometrics.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                editor_toggleBiometrics.putBoolean("onOrOff", true)
+                editor_toggleBiometrics.apply()
+                toggleBiometrics.isChecked = true
+            } else {
+                editor_toggleBiometrics.putBoolean("onOrOff", false)
+                editor_toggleBiometrics.apply()
+                toggleBiometrics.isChecked = false
+            }
+        }
 
         val mainMenuModifyLockedAppsBtn = findViewById<Button>(R.id.mainMenu_modifyLockedAppsBtn)
         mainMenuModifyLockedAppsBtn.setOnClickListener {
@@ -36,10 +50,6 @@ class MainMenuActivity : AppCompatActivity() {
             val toMasterPINActivity = Intent(this, MasterPINActivity::class.java)
             startActivity(toMasterPINActivity)
         }
-
-        //switch for fingerprint
-
-
     }
 
     override fun onBackPressed() {
